b9598623102ca1cfb443cc57e925f2d5c652edd5
0704203849 <conradsuuna.cs@gmail.com>
2017-07-28 14:21:12 +0300

    add functionality to the BottomBar.ts file and styling to the cs file

--- src/BottomBar/widget/BottomBar.ts
+++ src/BottomBar/widget/BottomBar.ts
@@ -5,18 +5,23 @@ import * as dojoStyle from "dojo/dom-style";
 import * as dojoClass from "dojo/dom-class";
 import * as dom from "dojo/dom";
 import * as dojohtml from "dojo/html";
+import * as $ from "jquery";
+// import * as JQuery from "BottomBar/lib/jquery-1.11.2";
 
 import "./ui/BottomBar.css";
 
 class BottomBar extends WidgetBase {
 
-    //parameters configured in the modeler from the xml file.
-    page1: string;
-    page2: string;
-    page3: string;
+    // parameters configured in the modeler from the xml file.
+
+    MicroflowToExecute1: string;
+    MicroflowToExecute2: string;
+    MicroflowToExecute3: string;
+    MicroflowToExecute4: string;
+    Usertext: string;
 
     // Internal variables. Non-primitives created in the prototype are shared between all widget instances.
-    //private handles: ;
+    // private handles: ;
     private contextObject: mendix.lib.MxObject;
 
     postCreate() {
@@ -40,18 +45,44 @@ class BottomBar extends WidgetBase {
 
         domConstruct.create("div", {
             class: "bar-item",
+            innerHTML: "<span>" + this.Usertext + "</span>"
+        }, bottomBar).addEventListener("click", () => {
+            if (this.MicroflowToExecute1) {
+                this.executeMicroflow(this.MicroflowToExecute1, this.contextObject.getGuid());
+            }
+        }, false);
+
+        domConstruct.create("div", {
+            class: "bar-item two",
             innerHTML: "<span>name</span>"
-        }, bottomBar);
+        }, bottomBar).addEventListener("click", () => {
+            if (this.MicroflowToExecute2) {
+                this.executeMicroflow(this.MicroflowToExecute2, this.contextObject.getGuid());
+            }
+        }, false);
 
         domConstruct.create("div", {
             class: "bar-item",
             innerHTML: "<span>name</span>"
-        }, bottomBar);
+        }, bottomBar).addEventListener("click", () => {
+            if (this.MicroflowToExecute3) {
+                this.executeMicroflow(this.MicroflowToExecute3, this.contextObject.getGuid());
+            }
+        }, false);
 
         domConstruct.create("div", {
             class: "bar-item",
             innerHTML: "<span>name</span>"
-        }, bottomBar);
+        }, bottomBar).addEventListener("click", () => {
+            if (this.MicroflowToExecute4) {
+                this.executeMicroflow(this.MicroflowToExecute4, this.contextObject.getGuid());
+            }
+        }, false);
+
+        /*  $("widget-bottom-bar > .bar-item").click(function () {
+              $(this).removeClass("widget-bottom-bar > .bar-item:active");
+              $(this).addClass("widget-bottom-bar > .bar-item:active");
+          });*/
     }
 
     private updateRendering() {
@@ -72,9 +103,28 @@ class BottomBar extends WidgetBase {
             });
         }
     }
+
+    private executeMicroflow(microflow: string, guid: string, cb?: (obj: mendix.lib.MxObject) => void) {
+        if (microflow && guid) {
+            mx.ui.action(microflow, {
+                params: {
+                    applyto: "selection",
+                    guids: [ guid ]
+                },
+                callback: (objs: mendix.lib.MxObject) => {
+                    if (cb && typeof cb === "function") {
+                        cb(objs);
+                    }
+                },
+                error: (error) => {
+                    mx.ui.error("Error executing microflow " + microflow + " : " + error.message);
+                }
+            }, this);
+        }
+    }
 }
 
-dojoDeclare("BottomBar.widget.BottomBar", [WidgetBase], function(Source: any) {
+dojoDeclare("BottomBar.widget.BottomBar", [WidgetBase], function (Source: any) {
     const result: any = {};
     for (const i in Source.prototype) {
         if (i !== "constructor" && Source.prototype.hasOwnProperty(i)) {
